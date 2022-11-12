76b9a41d156f564c63e2bd35efb31e7636999c1f
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2022-11-12 21:39:59 +0200

    CheckPermutation

---
+++ src/chapterone/CheckPermutation.java
@@ -0,0 +1,23 @@
+package chapterone;
+
+import java.util.Arrays;
+
+public class CheckPermutation {
+    public static void main(String[] args) {
+        System.out.println(checkPermutation("salah", "hasal"));
+        System.out.println(checkPermutation("salah", "hasnl"));
+    }
+
+    static boolean checkPermutation(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        return sort(str1).equals(sort(str2));
+    }
+
+    static String sort(String str) {
+        char[] c = str.toCharArray();
+        Arrays.sort(c);
+        return Arrays.toString(c);
+    }
+}
